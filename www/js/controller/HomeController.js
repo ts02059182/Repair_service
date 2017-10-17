f059429e613c752e81deb4859b3b58f0f0b45158
Thanawat Aruni <thanawat.aru@mocap.local>
2017-10-17 12:17:28 +0700

    update loading map

--- www/js/controller/HomeController.js
+++ www/js/controller/HomeController.js
@@ -1,8 +1,18 @@
-app.controller('HomeController', ['$scope','$cordovaOauth', '$state', '$http', '$cordovaNetwork', '$ionicPlatform', '$location', '$ionicHistory', '$ionicPopup', '$cordovaCamera', '$cordovaDevice', '$stateParams', function($scope,$cordovaOauth, $state, $http, $cordovaNetwork, $ionicPlatform, $location, $ionicHistory, $ionicPopup, $cordovaCamera, $cordovaDevice, $stateParams) {
+app.controller('HomeController', ['$scope','$cordovaOauth', '$state', '$http', '$cordovaNetwork', '$ionicPlatform', '$location', '$ionicHistory', '$ionicPopup', '$cordovaCamera', '$cordovaDevice', '$stateParams', '$timeout', '$ionicLoading', function($scope,$cordovaOauth, $state, $http, $cordovaNetwork, $ionicPlatform, $location, $ionicHistory, $ionicPopup, $cordovaCamera, $cordovaDevice, $stateParams, $timeout, $ionicLoading) {
 
 	$scope.tomap = function() {
 
-    $state.go('menu.map');
+		$ionicLoading.show({
+            content: 'Loading',
+            animation: 'fade-in',
+            showBackdrop: true,
+            maxWidth: 200,
+            showDelay: 0
+          });
+
+		$timeout(function () {
+    		$state.go('menu.map');
+    	  }, 2000);
 
   }
 
--- www/js/controller/MapController.js
+++ www/js/controller/MapController.js
@@ -1,34 +1,55 @@
-app.controller('MapController', ['$scope','$cordovaOauth', '$state', '$http', '$cordovaNetwork', '$ionicPlatform', '$location', '$ionicHistory', '$ionicPopup', '$cordovaCamera', '$cordovaDevice', '$stateParams', '$ionicLoading', '$compile', function($scope,$cordovaOauth, $state, $http, $cordovaNetwork, $ionicPlatform, $location, $ionicHistory, $ionicPopup, $cordovaCamera, $cordovaDevice, $stateParams, $ionicLoading, $compile,$cordovaGeolocation) {
+app.controller('MapController', ['$scope','$cordovaOauth', '$state', '$http', '$cordovaNetwork', '$ionicPlatform', '$location', '$ionicHistory', '$ionicPopup', '$cordovaCamera', '$cordovaDevice', '$stateParams', '$timeout', '$ionicLoading', function($scope,$cordovaOauth, $state, $http, $cordovaNetwork, $ionicPlatform, $location, $ionicHistory, $ionicPopup, $cordovaCamera, $cordovaDevice, $stateParams, $timeout, $ionicLoading) {
 
-  $ionicPlatform.ready(function() {    
- 
+  $ionicPlatform.ready(function(timeout,ionicLoading) {
+    
+
+    var im = 'file:///android_asset/www/img/marker2.png';
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+    function onSuccess(position,timeout,ionicLoading) {
+      latvalue = position.coords.latitude;
+      lngvalue = position.coords.longitude;
+      timestamp = position.timestamp;
+      var myLatlng = new google.maps.LatLng(latvalue, lngvalue);
+
+      var mapOptions = {
+        center: myLatlng,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        panControl: true,
+        zoomControl: true,
+        mapTypeControl: true,
+        scaleControl: true,
+        streetViewControl: true,
+        overviewMapControl: true,
+        rotateControl: true
+      };
+
+      var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      var userMarker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        icon: im
+      });
+
+      $scope.map = map;
+
+      $timeout(function() {
+      $ionicLoading.hide();
+
+      }, 2000);
 
 
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-
-        function onSuccess(position) {
-        latvalue = position.coords.latitude;
-        lngvalue = position.coords.longitude;
-        timestamp = position.timestamp;
-        var myLatlng = new google.maps.LatLng(latvalue, lngvalue);
-             
-            var mapOptions = {
-                center: myLatlng,
-                zoom: 16,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };          
-             
-            var map = new google.maps.Map(document.getElementById("map"), mapOptions);          
-             
-            $scope.map = map;
-
+      
     }
 
     function onError(error) {
-        alert(error);
+      alert(error);
     }
- 
-       
-    });
+
+
+  });
+
+
 
 }])
