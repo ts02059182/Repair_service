c4a28739827ddcc7eb51b8a5cc0925723fafdca0
Prakasit Maksin <prakasit.mak@mocap.local>
2017-10-30 15:47:32 +0700

    master tecmenu
    state.

--- www/js/controller/MasterController.js
+++ www/js/controller/MasterController.js
@@ -51,7 +51,7 @@ app.controller('MasterController', ['$scope','$cordovaOauth', '$state', '$http',
         if (data == 2) {
           $state.go('menu.home');
         } else if ( data ==3) {
-          $state.go('menu.hometec');
+          $state.go('menutec.hometec');
         }
 
         else {
