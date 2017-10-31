e426db9e5f38f4559438d6081e58ee4a079b9039
Thanawat Aruni <thanawat.aru@mocap.local>
2017-10-31 16:29:18 +0700

    update profiles

--- www/js/controller/ProfilesController.js
+++ www/js/controller/ProfilesController.js
@@ -1,6 +1,21 @@
-app.controller('ProfilesController', ['$scope', '$cordovaOauth', '$state', '$http', '$cordovaNetwork', '$ionicPlatform', '$location', '$ionicHistory', '$ionicPopup', '$cordovaCamera', '$cordovaDevice', '$stateParams', '$timeout', '$ionicLoading', function($scope, $cordovaOauth, $state, $http, $cordovaNetwork, $ionicPlatform, $location, $ionicHistory, $ionicPopup, $cordovaCamera, $cordovaDevice, $stateParams, $timeout, $ionicLoading) {
+app.controller('ProfilesController', ['$scope', '$cordovaOauth', '$state', '$http', '$cordovaNetwork', '$ionicPlatform', '$location', '$ionicHistory', '$ionicPopup', '$cordovaCamera', '$cordovaDevice', '$stateParams', '$timeout', '$ionicLoading', '$rootScope', function($scope, $cordovaOauth, $state, $http, $cordovaNetwork, $ionicPlatform, $location, $ionicHistory, $ionicPopup, $cordovaCamera, $cordovaDevice, $stateParams, $timeout, $ionicLoading,$rootScope) {
 
-  
+  var request = $http({
+    method: "post",
+    url: "http://61.91.124.155/repairservice_api/getprofile.php",
+    crossDomain: true,
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    data: {
+      username: $rootScope.username,
+      password: $rootScope.password
+    },
+  });
+
+  request.success(function(response) {
+    $scope.profile = response.profile;
+  });
+
+
 
   $ionicPlatform.registerBackButtonAction(function(event) {
     if ($ionicHistory.currentStateName() == 'menu.Profiles') {
