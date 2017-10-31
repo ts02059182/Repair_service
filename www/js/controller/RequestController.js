0b91c6093e9d8efb3aec920a2ab0fdfad7477458
Thanawat Aruni <thanawat.aru@mocap.local>
2017-10-31 12:29:51 +0700

    update upload image

--- www/js/controller/RequestController.js
+++ www/js/controller/RequestController.js
@@ -1,6 +1,6 @@
 app.controller('RequestController', ['$scope', '$cordovaOauth', '$state', '$http', '$cordovaNetwork', '$ionicPlatform', '$location', '$ionicHistory', '$ionicPopup', '$cordovaCamera', '$cordovaDevice', '$stateParams', '$timeout', '$ionicLoading', '$rootScope', function($scope, $cordovaOauth, $state, $http, $cordovaNetwork, $ionicPlatform, $location, $ionicHistory, $ionicPopup, $cordovaCamera, $cordovaDevice, $stateParams, $timeout, $ionicLoading, $rootScope) {
   $scope.tec_id = $state.params.tec_id;
-
+  var i = 0;
   var request = $http({
     method: "post",
     url: "http://61.91.124.155/repairservice_api/getuser.php",
@@ -48,10 +48,9 @@ app.controller('RequestController', ['$scope', '$cordovaOauth', '$state', '$http
 
     };
     if (img == 1) {
-      console.log("test");
+
       $cordovaCamera.getPicture(options).then(function(imageData) {
         
-        console.log(imageData);
         $scope.imgURI1 = imageData;
         $scope.nameimg1 = imageData.substr(imageData.lastIndexOf('/') + 1);
       }, function(err) {});
@@ -92,17 +91,23 @@ app.controller('RequestController', ['$scope', '$cordovaOauth', '$state', '$http
 
     request.success(function(data) {
       if (data > 0) {
-        $ionicPopup.alert({
-          title: 'Data Send Success',
-          template: 'You have successfully send request'
-        });
-        $ionicHistory.nextViewOptions({
-          disableBack: true
-        });
-         $state.go('menu.home');
-        // if($scope.imgURI1 != undefined){
-        //     uploadimg(1);
-        // }
+        // $ionicPopup.alert({
+        //   title: 'Data Send Success',
+        //   template: 'You have successfully send request'
+        // });
+        // $ionicHistory.nextViewOptions({
+        //   disableBack: true
+        // });
+        //  $state.go('menu.home');
+        if($scope.imgURI1 != undefined){
+            uploadimg(1,data);
+        }
+        if($scope.imgURI2 != undefined){
+            uploadimg(2,data);
+        }
+        if($scope.imgURI3 != undefined){
+            uploadimg(3,data);
+        }
       } else {
         $ionicPopup.alert({
           title: 'Data Send Failure',
@@ -113,9 +118,77 @@ app.controller('RequestController', ['$scope', '$cordovaOauth', '$state', '$http
 
   }
 
-  // $scope.uploadimg = function(img) {
-  //     alert($scope.imgURI+img);
+  function uploadimg(img,data) {
+    if(img == 1){
+      var options = new FileUploadOptions();
+            options.fileKey = "file";
+            options.fileName = $scope.nameimg1;
+            options.mimeType = "image/jpeg";
+            options.chunkedMode = false;
+            options.httpMethod = 'POST';
 
-  // }
+            var params = {};
+            params.value1 = data;
+            params.value2 = img;
+            options.params = params;
+
+
+            var ft = new FileTransfer();
+            ft.upload($scope.imgURI1, "http://61.91.124.155/repairservice_api/upload.php", win, fail, options);
+    }else if(img == 2){
+      var options = new FileUploadOptions();
+            options.fileKey = "file";
+            options.fileName = $scope.nameimg2;
+            options.mimeType = "image/jpeg";
+            options.chunkedMode = false;
+            options.httpMethod = 'POST';
+
+            var params = {};
+            params.value1 = data;
+            params.value2 = img;
+            options.params = params;
+
+
+            var ft = new FileTransfer();
+            ft.upload($scope.imgURI2, "http://61.91.124.155/repairservice_api/upload.php", win, fail, options);
+    }else if(img == 3){
+      var options = new FileUploadOptions();
+            options.fileKey = "file";
+            options.fileName = $scope.nameimg3;
+            options.mimeType = "image/jpeg";
+            options.chunkedMode = false;
+            options.httpMethod = 'POST';
+
+            var params = {};
+            params.value1 = data;
+            params.value2 = img;
+            options.params = params;
+
+
+            var ft = new FileTransfer();
+            ft.upload($scope.imgURI3, "http://61.91.124.155/repairservice_api/upload.php", win, fail, options);
+    }
+
+  }
+
+  function win(r) {
+    //console.log(r.response);
+        if(i == 0){
+        i = 1;
+        $ionicPopup.alert({
+          title: 'Data Send Success',
+          template: 'You have successfully send request'
+        });
+        $ionicHistory.nextViewOptions({
+          disableBack: true
+        });
+         $state.go('menu.home');
+      }
+  }
+
+  function fail(error) {
+    alert("An error has occurred while uploading: Code = " + error.code);
+
+  }
 
 }])
