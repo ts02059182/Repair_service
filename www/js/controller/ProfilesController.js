47f71e04c41103a86b755b9a10e18aaefa3d3a54
MOCAP\thanawat.aru <thanawat.f@gmail.com>
2017-11-01 23:19:19 +0700

    update profiles

--- www/js/controller/ProfilesController.js
+++ www/js/controller/ProfilesController.js
@@ -17,6 +17,63 @@ app.controller('ProfilesController', ['$scope', '$cordovaOauth', '$state', '$htt
 
 
 
+  $scope.toedit = function(group) {
+    $scope.Edit = {};
+    $scope.Editdata = {};
+    $scope.cusid = group.id;
+    $scope.fname = group.fname;
+    $scope.lname = group.lname;
+    $scope.phoen = group.phone;
+    $scope.email = group.email;
+
+    $ionicPopup.show({
+      template: '<form name="myform"> <div class="list padding hint"><ion-item class="item-floating-label" style="background: White ; margin-top: 2%;" ng-repeat="group in profile" ng-if="group.id == cusid "> <span class="input-label">First Name</span> <input type="text" name="name" placeholder="First Name" ng-model="Editdata.name" ng-init="Editdata.name = group.fname"> </ion-item> <ion-item class="item-floating-label" style="background: White ; margin-top: 2%;" ng-repeat="group in profile" ng-if="group.id == cusid "> <span class="input-label">Last Name</span> <input type="text" name="lname" placeholder="Last Name" ng-model="Editdata.lname" ng-init="Editdata.lname = group.lname"> </ion-item> <ion-item class="item-floating-label" style="background: White ; margin-top: 2%;" ng-repeat="group in profile" ng-if="group.id == cusid "> <span class="input-label">Phone</span> <input type="text" name="phone" placeholder="Phone" ng-model="Editdata.phone" ng-init="Editdata.phone = group.phone"> </ion-item> <ion-item class="item-floating-label" style="background: White ; margin-top: 2%;" ng-repeat="group in profile" ng-if="group.id == cusid "> <span class="input-label">Email</span> <input type="text" name="email" placeholder="Email" ng-model="Editdata.email" ng-init="Editdata.email = group.email"> </ion-item></div> </form>',
+      title: 'Edit Profiles info',
+      scope: $scope,
+      buttons: [
+        { text: '<b>Cancel</b>' }, {
+          text: '<b>Save</b>',
+          type: 'button-positive',
+          onTap: function(e) {
+            
+            var request = $http({
+              method: "post",
+              url: "http://61.91.124.155/repairservice_api/editprofile.php",
+              crossDomain: true,
+              headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+              data: {
+                name: $scope.Editdata.name,
+                lname: $scope.Editdata.lname,
+                phone: $scope.Editdata.phone,
+                email: $scope.Editdata.email,
+                id: group.id
+
+              },
+            });
+            request.success(function(data) {
+              if (data > 0) {
+                $ionicPopup.alert({
+                  title: 'Profiles Information Success',
+                  template: 'The Profiles information has been successfully Edited'
+                });
+              } else {
+                $ionicPopup.alert({
+                  title: 'Profiles Information Failure',
+                  template: 'The Profiles information has been unsuccessfully Edited'
+                });
+              }
+
+            });
+            $state.go($state.current, {}, { reload: true });
+
+          }
+        }
+      ]
+    });
+
+  }
+
+
   $ionicPlatform.registerBackButtonAction(function(event) {
     if ($ionicHistory.currentStateName() == 'menu.Profiles') {
       //event.preventDefault();
