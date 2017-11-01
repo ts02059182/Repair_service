566668eb56e2ba017a53b7b504910ab2097322d0
MOCAP\thanawat.aru <thanawat.f@gmail.com>
2017-11-02 00:28:23 +0700

    update history

--- www/js/controller/HistoryController.js
+++ www/js/controller/HistoryController.js
@@ -1,6 +1,35 @@
-app.controller('HistoryController', ['$scope', '$cordovaOauth', '$state', '$http', '$cordovaNetwork', '$ionicPlatform', '$location', '$ionicHistory', '$ionicPopup', '$cordovaCamera', '$cordovaDevice', '$stateParams', '$timeout', '$ionicLoading', function($scope, $cordovaOauth, $state, $http, $cordovaNetwork, $ionicPlatform, $location, $ionicHistory, $ionicPopup, $cordovaCamera, $cordovaDevice, $stateParams, $timeout, $ionicLoading) {
+app.controller('HistoryController', ['$scope', '$cordovaOauth', '$state', '$http', '$cordovaNetwork', '$ionicPlatform', '$location', '$ionicHistory', '$ionicPopup', '$cordovaCamera', '$cordovaDevice', '$stateParams', '$timeout', '$ionicLoading', '$rootScope', function($scope, $cordovaOauth, $state, $http, $cordovaNetwork, $ionicPlatform, $location, $ionicHistory, $ionicPopup, $cordovaCamera, $cordovaDevice, $stateParams, $timeout, $ionicLoading, $rootScope) {
 
-  
+  var request = $http({
+    method: "post",
+    url: "http://61.91.124.155/repairservice_api/getuser.php",
+    crossDomain: true,
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    data: {
+      username: $rootScope.username,
+      password: $rootScope.password
+    },
+  });
+
+  request.success(function(data) {
+    $scope.user_id = data;
+    var request1 = $http({
+      method: "post",
+      url: "http://61.91.124.155/repairservice_api/gethistory.php",
+      crossDomain: true,
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      data: {
+        id: $scope.user_id
+      },
+    });
+
+    request1.success(function(response) {
+      console.log(response);
+      $scope.history = response.history;
+    });
+  });
+
+
 
   $ionicPlatform.registerBackButtonAction(function(event) {
     if ($ionicHistory.currentStateName() == 'menu.history') {
