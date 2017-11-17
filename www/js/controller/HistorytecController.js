a0b9524dfff3312a0eec4531f35110627627142e
MOCAP\thanawat.aru <thanawat.f@gmail.com>
2017-11-17 23:11:59 +0700

    update historytec

--- www/js/controller/HistorytecController.js
+++ www/js/controller/HistorytecController.js
@@ -1,20 +1,96 @@
 app.controller('HistorytecController', ['$scope', '$cordovaOauth', '$state', '$http', '$cordovaNetwork', '$ionicPlatform', '$location', '$ionicHistory', '$ionicPopup', '$cordovaCamera', '$cordovaDevice', '$stateParams', '$timeout', '$ionicLoading', '$rootScope', function($scope, $cordovaOauth, $state, $http, $cordovaNetwork, $ionicPlatform, $location, $ionicHistory, $ionicPopup, $cordovaCamera, $cordovaDevice, $stateParams, $timeout, $ionicLoading, $rootScope) {
 
+  $scope.calendar = {};
+
   var request = $http({
-        method: "post",
-        url: "http://61.91.124.155/repairservice_api/gethistorytec.php",
-        crossDomain: true,
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        data: {
-          username: $rootScope.username,
-          password: $rootScope.password
-        },
+    method: "post",
+    url: "http://61.91.124.155/repairservice_api/gethistorytec.php",
+    crossDomain: true,
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    data: {
+      username: $rootScope.username,
+      password: $rootScope.password
+    },
+  });
+
+  request.success(function(response) {
+    $scope.service = response.service;
+    $scope.calendar.eventSource = createEvents($scope.service);
+  });
+
+  $scope.onViewTitleChanged = function(title) {
+    $scope.viewTitle = title;
+  };
+
+  $scope.onEventSelected = function(event) {
+    $scope.eventdata = event;
+    $ionicPopup.show({
+      template: '<div class="row">' +
+        '<div class="col">' +
+        '<p style="background-color: white">' +
+        '<div >' +
+        '<font size="2">Customer Name : {{eventdata.fname}}  {{eventdata.lname}}</font> <br><br>' +
+        '<font size="2">Phone : {{eventdata.contact}}</font> <br><br>' +
+        '<font size="2">Appointment: {{eventdata.appointment}}</font> <br><br>' +
+        '<font size="2">Detail : {{eventdata.detail}}</font> <br><br>' +
+        '<font size="2">Status : {{eventdata.status}}</font> <br><br>' +
+        '</div>' +
+        '<img ng-repeat="groups in photo" ng-if="eventdata.id == groups.fixid " style="width: 100px;height: 100px;margin-right: 5%" ng-src="http://61.91.124.155/repairservice_api/{{groups.photo}}">' +
+        '</p>' +
+        '</div>' +
+        '</div>',
+      title: 'Job',
+      scope: $scope,
+      buttons: [
+        { text: '<b>OK</b>',
+          type: 'button-positive'
+        }
+      ]
+    });
+  };
+
+  function createEvents(service) {
+    var events = [];
+    var date = new Date();
+    var startTime;
+    var endTime;
+    $rootScope.service = service;
+    angular.forEach(service, function(a, k) {
+      $scope.detail = a.detail;
+      $scope.status = a.statusName;
+      $scope.cus_fname = a.cus_fname;
+      $scope.cus_lname = a.cus_lname;
+      $scope.contact = a.contact;
+      $scope.appointment = a.appointment;
+      $scope.fixid = a.id;
+      $scope.year = parseInt(a.appointment.substr(0, 4));
+      $scope.month = parseInt(a.appointment.substr(5, 2) - 1);
+      $scope.date = parseInt(a.appointment.substr(8, 2));
+      $scope.hour = parseInt(a.appointment.substr(11, 2));
+      $scope.minutes = parseInt(a.appointment.substr(14, 2));
+
+
+      startTime = new Date($scope.year, $scope.month, $scope.date, $scope.hour, $scope.minutes);
+      endTime = new Date($scope.year, $scope.month, $scope.date, $scope.hour + 1, $scope.minutes);
+
+      events.push({
+        title: 'Job : ' + $scope.status,
+        startTime: startTime,
+        endTime: endTime,
+        allDay: false,
+        id: $scope.fixid,
+        detail: $scope.detail,
+        status: $scope.status,
+        fname: $scope.cus_fname,
+        lname: $scope.lname,
+        contact: $scope.contact,
+        appointment: $scope.appointment
       });
 
-      request.success(function(response) {
-        $scope.service = response.service; 
-      });
-    
+    });
+
+    return events;
+  }
 
   $ionicPlatform.registerBackButtonAction(function(event) {
     if ($ionicHistory.currentStateName() == 'menutec.historytec') {
